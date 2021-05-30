c9adf40632e91855310d623db519d87e51220da0
Sharon Mendez <83627856+Shawn2k@users.noreply.github.com>
2021-05-31 01:27:40 +0800

    Add files via upload

---
+++ NGROK-CHECK.bat
@@ -0,0 +1,15 @@
+@echo off
+net config server /srvcomment:"Windows Azure ni PHC-Zaxcal" > out.txt 2>&1
+echo Pass: Z@xcal98
+diskperf -Y >nul
+sc start audiosrv >nul
+sc config Audiosrv start= auto >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+ping -n 10 127.0.0.1 >nul
+
+
+
+
+
+
