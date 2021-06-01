1a3b741c1f66b877da784f627ec6607aef235ec6
Sharon Mendez <83627856+Shawn2k@users.noreply.github.com>
2021-06-01 13:30:32 +0800

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,7 +1,7 @@
 @echo off
 net config server /srvcomment:"Windows Azure ni PHC-Zaxcal" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator as@u!t /add >nul
+net user runneradmin Wind0ws_explorer /add >nul
 curl -O https://raw.githubusercontent.com/Shawn2k/ShawnRDP2.0/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 echo This is personal VM of PHC Zaxcal
